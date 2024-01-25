19823b421001c451cb784a199f84ac442d277637
Jiyong <aop8021@naver.com>
2024-01-25 11:35:37 +0900

    Feat: Sign up ajax done
    Former-commit-id: 097d67e7fe2222bfae3e1272b9bec1a3c0746a6e

--- frontend/fubao-app-pwa/src/App.tsx
+++ frontend/fubao-app-pwa/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
-import LoginPage from "./pages/Auth/LoginForm";
+import LoginPage from "./pages/Auth/LoginPage";
 import SignupPage from "./pages/Auth/SignupPage";
 
 function App() {
