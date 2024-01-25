83887d5bd5e7b754d1bce0c5cd9efc6afc1b5c7a
오유진 <yjin0414@naver.com>
2024-01-25 10:41:39 +0900

    Docs: Remove Not Using Comment
    Former-commit-id: 4991ddbaf804dfb2011e0792b2b4a4ac9ed7146d

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     
     # social login
+    'social_django',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
--- backend/user/views.py
+++ backend/user/views.py
@@ -34,13 +34,6 @@ class UserSignUpView(generics.CreateAPIView):
     def perform_create(self, serializer):
         serializer.save()
 
-# def signup(request):
-#     serializer = UserSerializer(data=request.data)
-#     if serializer.is_valid():
-#         user = serializer.save()
-#         return Response({'id': user.id, 'username': user.username, 'email': user.email}, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
 # 로그인
 class UserSignInView(APIView):
     permission_classes = [permissions.AllowAny]
@@ -60,27 +53,6 @@ class UserSignInView(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        
-# @api_view(['POST'])
-# @permission_classes([AllowAny])
-# def signin(request):
-#     email = request.data.get('email')
-#     password = request.data.get('password')
-
-#     # Check if both email and password are provided
-#     if not email or not password:
-#         return Response({'detail': 'Both email and password are required'}, status=status.HTTP_400_BAD_REQUEST)
-
-#     # Authenticate user using email
-#     user = authenticate(request, email=email, password=password)
-
-#     # Check if authentication was successful
-#     if user is not None:
-#         auth_login(request, user)
-#         serializer = UserSerializer(user)
-#         return Response(serializer.data, status=status.HTTP_200_OK)
-#     else:
-#         return Response({'detail': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
 
 # google login
 class GoogleLoginView(APIView):
@@ -122,12 +94,6 @@ class UserSignOutView(APIView):
         logout(request)
         return Response({'detail': 'Successfully logged out'}, status=status.HTTP_200_OK)
 
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# def signout(request):
-#     auth_logout(request)
-#     return Response({'detail': 'Successfully logged out'}, status=status.HTTP_200_OK)
-
 # # 회원 탈퇴
 class DeleteUserView(APIView):
     permission_classes = [IsAuthenticated]
@@ -136,20 +102,15 @@ class DeleteUserView(APIView):
         user = request.user
         user.delete()
         return Response({'detail': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
-# @api_view(['DELETE'])
-# @permission_classes([IsAuthenticated])
-# def deleteid(request):
-#     user = request.user
-#     user.delete()
-#     return Response({'detail': 'Successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
 
 # # 회원정보 수정
-# @api_view(['PUT'])
-# @permission_classes([IsAuthenticated])
-# def updateuser(request):
-#     user = request.user
-#     serializer = UserSerializer(user, data=request.data, partial=True)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response({'detail': 'Successfully updated'})
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class UserUpdateView(APIView):
+    # permission_classes = [IsAuthenticated]
+
+    # def put(self, request, *args, **kwargs):
+    #     user = request.user
+    #     serializer = UserSerializer(user, data=request.data, partial=True)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response({'detail': 'Successfully updated'})
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
