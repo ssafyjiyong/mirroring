1304722ffb9b4bb75d8b61bcdb3f23a06aa3cf5e
오유진 <yjin0414@naver.com>
2024-01-26 15:03:49 +0900

    Feat: Modify User
    Former-commit-id: 4a6925411df2544f9d957e9b7b56a735cd798a31

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,19 +1,8 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from .views import CustomRegisterView
 
 urlpatterns = [
-    # 회원 가입
-    path('signup/', views.signup, name='signup'),
-    
-    # 로그인
-    path('signin/', views.signin, name='signin'),
-
-    # 로그아웃
-    path('signout/', views.signout, name='signout'),
-    
-    # 회원 탈퇴
-    path('deleteid/', views.deleteid, name='deleteid'),
-    
-    # 회원정보 수정
-    path('updateuser/', views.updateuser, name='updateuser'),
+    path('', include('dj_rest_auth.urls')),
+    # path('registration/', include('dj_rest_auth.registration.urls')),
+    path('register/', CustomRegisterView.as_view(), name='rest_register')
 ]
