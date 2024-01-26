16586488455467e8f1e7262abcccdc550991317d
오유진 <yjin0414@naver.com>
2024-01-26 10:05:49 +0900

    Fix: Fix Basic User
    Former-commit-id: deab102b4e9999284c0fb682d0ff1517ff1ce740

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,45 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+from .managers import UserManager
 
 # Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-
-        user = User(email=email, **extra_fields)
-
-        if password:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError("Superuser must have is_staff=True.")
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError("Superuser must have is_superuser=True.")
-
-        return self.create_user(email, password, **extra_fields)
-
-
 class User(AbstractUser):
     GENDER_CHOICES = [
         ('선택하지 않음', '선택하지 않음'),
         ('남성', '남성'),
         ('여성', '여성'),
     ]
-
     username = None
     first_name = None
     last_name = None
     
+    age = models.IntegerField(null=True)
+    name = models.CharField(max_length=10)
     email = models.EmailField(unique=True)
     gender = models.CharField(
         max_length=10, 
@@ -47,12 +24,13 @@ class User(AbstractUser):
         default='선택하지 않음'
     )
     date_of_birth = models.DateField(blank=True, null=True)
-
     nickname = models.CharField(null=True, unique=True, max_length=8)
-
     profile_img = models.ImageField(upload_to='profile_images/', null=True, blank=True)
 
     objects = UserManager()
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    
+    def __str__(self):
+        return self.email
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,25 +1,8 @@
-from django.urls import path
-from . import views
-
-app_name = 'user'
+from django.urls import path, include
+from .views import CustomRegisterView
 
 urlpatterns = [
-    # 회원 가입
-    path('signup/', views.UserSignUpView.as_view(), name='signup'),
-    
-    # # 로그인
-    path('signin/', views.UserSignInView.as_view(), name='signin'),
-
-    # 구글 소셜로그인
-    path('google/login/', views.GoogleLoginView.as_view(), name='google_login'),
-    path('google/callback/', views.GoogleCallbackView.as_view(), name='google_callback'),
-
-    # 로그아웃
-    path('signout/', views.UserSignOutView.as_view(), name='signout'),
-    
-    # 회원 탈퇴
-    path('deleteid/', views.DeleteUserView.as_view(), name='deleteid'),
-    
-    # # 회원정보 수정
-    # path('updateuser/', views.UserUpdateView.as_view(), name='updateuser'),
+    path('', include('dj_rest_auth.urls')),
+    # path('registration/', include('dj_rest_auth.registration.urls')),
+    path('register/', CustomRegisterView.as_view(), name='rest_register')
 ]
