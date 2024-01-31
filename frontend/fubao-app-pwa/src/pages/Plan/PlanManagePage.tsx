dbb3f7646199568f6d04ba2a26a340607cdb8a31
Jiyong <aop8021@naver.com>
2024-01-31 17:59:27 +0900

    Temp: Planmanage working
    Former-commit-id: c07affa815bdfcc8f1d467c478fc0446535fa980

--- frontend/fubao-app-pwa/src/pages/Plan/PlanManagePage.tsx
+++ frontend/fubao-app-pwa/src/pages/Plan/PlanManagePage.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../FontAwsome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styled from "styled-components";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0.4em;
+`;
+
+const RegisterBox = styled.div`
+  height: 35em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 const PlanManagePage = () => {
-  return (
-    <div>PlanManagePage</div>
-  )
-}
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
-export default PlanManagePage
+  return (
+    <Container>
+      <p style={{ fontSize: "1.5em", fontWeight: "bold" }}>일정등록</p>
+      {/* 일정 등록 칸 */}
+      <RegisterBox>
+        <div>
+          <span>일정: </span>
+          <DatePicker
+            dateFormat="yyyy.MM.dd" // 날짜 형태
+            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+            minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
+            maxDate={new Date()} // maxDate 이후 날짜 선택 불가
+            selected={selectedDate}
+            onChange={(date) => setSelectedDate(date)}
+          />
+        </div>
+        <div>
+        <span>장소: </span>
+        <input type="text" placeholder="장소 (예시 부산항)" />
+        </div>
+        <div>
+        <span>포인트: </span>
+        <input type="text" placeholder="포인트 (예시 방파제)" />
+        </div>
+        <div>
+        <span>방법: </span>
+        <input type="text" placeholder="방법 (예시 찌낚시)" />
+        </div>
+      </RegisterBox>
+    </Container>
+  );
+};
+
+export default PlanManagePage;
