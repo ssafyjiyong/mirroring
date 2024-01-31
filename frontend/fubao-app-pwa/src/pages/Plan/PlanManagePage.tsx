ff66999626299b2ceaa65e849623a586e05da316
Jiyong Kim <aop8021@naver.com>
2024-01-31 21:38:32 +0900

    Implement: Plan manage page wireframe done
    Former-commit-id: e43a9c744d8618b926f37bf59eff503c31853326

--- frontend/fubao-app-pwa/src/pages/Plan/PlanManagePage.tsx
+++ frontend/fubao-app-pwa/src/pages/Plan/PlanManagePage.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import "../../FontAwsome";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import Button from "@mui/joy/Button";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import "./styles.css";
 
 const Container = styled.div`
   display: flex;
@@ -20,6 +20,31 @@ const RegisterBox = styled.div`
   align-items: center;
 `;
 
+const Input = styled.input`
+  border-radius: 10px;
+  width: 20em;
+  padding: 0.5em;
+  margin: 0.5em 0;
+  border: 1px solid #ccc;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  font-size: 1em;
+  ::placeholder {
+    color: #ccc;
+  }
+`;
+
+const Span = styled.span`
+  display: inline-block;
+  width: 20em;
+  margin-right: 0.5em;
+`;
+
+const AlignDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const PlanManagePage = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
 
@@ -27,31 +52,42 @@ const PlanManagePage = () => {
     <Container>
       <p style={{ fontSize: "1.5em", fontWeight: "bold" }}>일정등록</p>
       {/* 일정 등록 칸 */}
-      <RegisterBox>
-        <div>
-          <span>일정: </span>
-          <DatePicker
-            dateFormat="yyyy.MM.dd" // 날짜 형태
-            shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
-            minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
-            maxDate={new Date()} // maxDate 이후 날짜 선택 불가
-            selected={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
-          />
-        </div>
-        <div>
-        <span>장소: </span>
-        <input type="text" placeholder="장소 (예시 부산항)" />
-        </div>
-        <div>
-        <span>포인트: </span>
-        <input type="text" placeholder="포인트 (예시 방파제)" />
-        </div>
-        <div>
-        <span>방법: </span>
-        <input type="text" placeholder="방법 (예시 찌낚시)" />
-        </div>
-      </RegisterBox>
+      <form action="">
+        <RegisterBox>
+          <AlignDiv>
+            <Span>일정: </Span>
+              <DatePicker
+                className="datePicker"
+                dateFormat="yyyy.MM.dd" // 날짜 형태
+                shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
+                minDate={new Date("2000-01-01")} // minDate 이전 날짜 선택 불가
+                maxDate={new Date()} // maxDate 이후 날짜 선택 불가
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+              />
+          </AlignDiv>
+          <AlignDiv>
+            <Span>장소: </Span>
+            <Input type="text" placeholder="장소 (예시 부산항)" />
+          </AlignDiv>
+          <AlignDiv>
+            <Span>포인트: </Span>
+            <Input type="text" placeholder="포인트 (예시 방파제)" />
+          </AlignDiv>
+          <AlignDiv>
+            <Span>방법: </Span>
+            <Input type="text" placeholder="방법 (예시 찌낚시)" />
+          </AlignDiv>
+          <Button
+            onClick={function () {}}
+            size="md"
+            variant="solid"
+            style={{ margin: "2em" }}
+          >
+            등록하기
+          </Button>
+        </RegisterBox>
+      </form>
     </Container>
   );
 };
