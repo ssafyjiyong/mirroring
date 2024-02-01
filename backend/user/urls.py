ed8b6cf5f7cb25a081663f1252767857bba3293b
오유진 <yjin0414@naver.com>
2024-02-01 20:37:30 +0900

    Fix: Fix 403 error
    Former-commit-id: b1b4f923977f75c36926b0b3ae5bab20a456d34e

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import CustomRegisterView, UserProfileView
+from .views import CustomRegisterView, UserProfileView, LoginView
 
 urlpatterns = [
     path('', include('dj_rest_auth.urls')),
+    # path('login/', LoginView.as_view(), name='login'),
     path('register/', include('dj_rest_auth.registration.urls')),
     # path('register/', CustomRegisterView.as_view(), name='rest_register'),
     path('profile/', UserProfileView.as_view(), name='profile'),
--- backend/user/views.py
+++ backend/user/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from rest_framework.generics import RetrieveUpdateAPIView
 from dj_rest_auth.registration.views import RegisterView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.authtoken.models import Token 
+from rest_framework.views import APIView 
+from rest_framework.authentication import TokenAuthentication 
 from .serializers import CustomRegisterSerializer, UserSerializer
 
 # Create your views here.
 class CustomRegisterView(RegisterView):
     serializer_class = CustomRegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(APIView):
+    def post(self, request):
+        user = authenticate(email=request.data['email'], password=request.data['password'])
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key})
+        else:
+            return Response({'error': 'Invalid credentials'}, status=401)
 
 class UserProfileView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
