7d6742326ef4a6d10f11925be6945f830da80f66
오유진 <yjin0414@naver.com>
2024-02-03 17:56:53 +0900

    Docs: Fix User Profile GET
    Former-commit-id: bcb7753d2c64661093ea7b60ae8bd1120d7dcf35

--- backend/user/views.py
+++ backend/user/views.py
@@ -3,7 +3,7 @@ from rest_framework.generics import RetrieveUpdateAPIView
 from dj_rest_auth.registration.views import RegisterView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CustomRegisterSerializer, UserSerializer
+from .serializers import CustomRegisterSerializer, UserSerializer, UserProfileSerializer
 
 # Create your views here.
 class CustomRegisterView(RegisterView):
@@ -11,20 +11,22 @@ class CustomRegisterView(RegisterView):
 
 class UserProfileView(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
-    serializer_class = UserSerializer
 
     def get_object(self):
         return self.request.user
 
     def get(self, request, *args, **kwargs):
+        self.serializer_class = UserProfileSerializer
         return self.retrieve(request, *args, **kwargs)
 
     def patch(self, request, *args, **kwargs):
+        self.serializer_class = UserSerializer
         return self.partial_update(request, *args, **kwargs)
 
     def put(self, request, *args, **kwargs):
+        self.serializer_class = UserSerializer
         return self.update(request, *args, **kwargs)
-    
+
     def delete(self, request, *args, **kwargs):
         user = self.get_object()
         user.delete()
