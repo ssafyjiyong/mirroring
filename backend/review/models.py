0959d4b4312c65b9053508160441bbbc21a00145
김민영 <jnu195260@gmail.com>
2024-02-05 17:29:48 +0900

    Add: add new location reviewAPI(post)
    Former-commit-id: 3026fdc4bb2475b3478dbaea07fa22af7adb2ca5

--- backend/review/models.py
+++ backend/review/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from user.models import User
 from information.models import fishing_method
+from location.models import location
 
 # Create your models here.
 class method_reivew(models.Model):
@@ -9,6 +10,10 @@ class method_reivew(models.Model):
     method=models.ForeignKey(fishing_method,on_delete=models.SET_NULL,null=True)
     weight=models.IntegerField(default=0)
     
+class location_review(models.Model):
+    user=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    location=models.ForeignKey(location,on_delete=models.SET_NULL,null=True)
+    weight=models.IntegerField(default=0)
 
 
 
