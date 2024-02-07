47f7d4feccd60576570c2d2cc5a60957ed66797e
오유진 <yjin0414@naver.com>
2024-02-07 21:09:49 +0900

    Add: Add Fish Images
    Former-commit-id: 007b38930495ae2fc0f1b179951d84b8b6cb9574

--- frontend/fubao-app-pwa/src/pages/Information/ReleaseInfo.tsx
+++ frontend/fubao-app-pwa/src/pages/Information/ReleaseInfo.tsx
@@ -9,6 +9,20 @@ type ReleaseInfo = {
   standard: number | null;
 };
 
+// 물고기 images
+const fishImages = [
+  { name: '참돔', image: '/imgs/fish/chamdom.png', link: '/detail/참돔'}, // 1. 참돔
+  { name: '농어', image: '/imgs/fish/nonguh.png', link: '/detail/농어'}, // 2. 농어
+  { name: '전갱이', image: '/imgs/fish/jeongang.png', link: '/detail/전갱이'}, // 3. 전갱이
+  { name: '숭어', image: '/imgs/fish/sunguh.png', link: '/detail/숭어'}, // 4. 숭어
+  { name: '고등어', image: '/imgs/fish/godeunguh.png', link: '/detail/고등어'}, // 5. 고등어
+  { name: '광어', image: '/imgs/fish/kwanguh.png', link: '/detail/광어'}, // 6. 광어
+  { name: '우럭', image: '/imgs/fish/wuroek.png', link: '/detail/우럭'}, // 7. 우럭
+  { name: '감성돔', image: '/imgs/fish/gamsungdom.png', link: '/detail/감성돔'}, // 8. 감성돔
+  { name: '돌돔', image: '/imgs/fish/doldom.png', link: '/detail/돌돔'}, // 9. 돌돔
+  { name: '쥐노래미', image: '/imgs/fish/gnoraemi.png', link: '/detail/쥐노래미'}, // 10. 쥐노래미
+];
+
 const ReleaseInfoPage = () => {
   // https://www.youtube.com/watch?v=8GZTUOAk5i0
   const [releaseInfo, setReleaseInfo] = useState<ReleaseInfo[]>([]); // 이곳에 API로부터 받아온 정보를 저장 (type지정해둔것도 사용!)
@@ -32,10 +46,15 @@ const ReleaseInfoPage = () => {
       <div style={{ padding: '1rem' }}>방생기준 표</div>
       <div>
         {releaseInfo.map((info) => (
-          <div key={info.id}>
-            <div>한국어 이름: {info.name_kor}</div>
-            <div>영어 이름: {info.name_eng}</div>
-            <div>방생기준: {info.standard ? info.standard : "잡아도 잡아도 씨가 마르지 않음"}</div>
+          <div key={info.id} style={{ padding: '1rem' }}>
+            <div>
+              <img src={fishImages[info.id - 1]?.image} alt={info.name_kor} style={{ width: '5.5rem', borderRadius: '10px' }} />
+            </div>
+            <div>
+              <div>한국어 이름: {info.name_kor}</div>
+              <div>영어 이름: {info.name_eng}</div>
+              <div>방생기준: {info.standard ? info.standard : "잡아도 잡아도 씨가 마르지 않음"}</div>
+            </div>
           </div>
         ))}
       </div>
