202cc212d2f6e76db8aeb399d4d3f12073703449
오유진 <yjin0414@naver.com>
2024-02-07 20:40:33 +0900

    Add: Modify Release Fish
    Former-commit-id: 11d9641a6350c197219733c171eb432f04234c38

--- frontend/fubao-app-pwa/src/pages/Information/ReleaseInfo.tsx
+++ frontend/fubao-app-pwa/src/pages/Information/ReleaseInfo.tsx
@@ -1,15 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { releaseFishApi } from "../../store/api";
 
-import axios from 'axios';
+// 아니 타입 지정 안되면 다 ^^^^하고 오류뜨네ㅠㅠㅠ
+type ReleaseInfo = {
+  id: number;
+  name_kor: string;
+  name_eng: string;
+  standard: number | null;
+};
 
 const ReleaseInfoPage = () => {
-  const [releaseInfo, setReleaseInfo] = useState<any[]>([]); // 이곳에 API로부터 받아온 정보를 저장
+  // https://www.youtube.com/watch?v=8GZTUOAk5i0
+  const [releaseInfo, setReleaseInfo] = useState<ReleaseInfo[]>([]); // 이곳에 API로부터 받아온 정보를 저장 (type지정해둔것도 사용!)
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const apiResponse = await releaseFishApi(); // releaseFishApi 호출
+        const token = localStorage.getItem('token'); // token 가져와서
+        const apiResponse = await releaseFishApi(token); // releaseFishApi 호출할 때 토큰 주기
         setReleaseInfo(apiResponse);
       } catch (error) {
         console.error('API 호출 중 오류 발생:', error);
@@ -22,11 +30,9 @@ const ReleaseInfoPage = () => {
   return (
     <>
       <div style={{ padding: '1rem' }}>방생기준 표</div>
-      <div>{releaseInfo}</div>
       <div>
         {releaseInfo.map((info) => (
           <div key={info.id}>
-            <div>ID: {info.id}</div>
             <div>한국어 이름: {info.name_kor}</div>
             <div>영어 이름: {info.name_eng}</div>
             <div>방생기준: {info.standard ? info.standard : "잡아도 잡아도 씨가 마르지 않음"}</div>
