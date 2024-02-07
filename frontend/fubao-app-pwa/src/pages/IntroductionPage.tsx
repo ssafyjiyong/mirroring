799f930cd31cc3337df4842639ea171380934080
Jiyong <aop8021@naver.com>
2024-02-08 02:37:08 +0900

    Implement: IntroductionPage working
    Former-commit-id: 248ec2f5a45724f3ee22e0198e3ec339cf4c28c8

--- frontend/fubao-app-pwa/src/pages/IntroductionPage.tsx
+++ frontend/fubao-app-pwa/src/pages/IntroductionPage.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Slider from "react-slick";
+
+const StyledSlider = styled(Slider)`
+  overflow: hidden;
+
+  .slick-dots {
+    bottom: 1px;
+  }
+`
 
 const IntroductionPage = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <div>IntroductionPage</div>
-  )
+    <div>
+      <StyledSlider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+      </StyledSlider>
+    </div>
+  );
 }
 
 export default IntroductionPage
