d28f20c21756dad622b366cc6e79a6adb2b4a6f3
김민영 <jnu195260@gmail.com>
2024-02-11 18:37:40 +0900

    Temp: update front api ai-url
    Former-commit-id: 12477ff0592bea65186b26bf6f5badab10c3b2d1

--- frontend/fubao-app-pwa/src/store/api.js
+++ frontend/fubao-app-pwa/src/store/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const API_URL = "http://127.0.0.1:8000";
 const API_URL = "https://i10c104.p.ssafy.io/api";
-const API_URL_FLASK = "http://54.180.108.229:5000";
+const API_URL_FLASK = "https://i10c104.p.ssafy.io/api/ai";
 
 // GET 요청 API
 export const currentUserApi = async (token) => {
