da3f5c2af19aee207da6934971098e926f98569f
김민영 <jnu195260@gmail.com>
2024-02-13 23:24:22 +0900

    Temp: add information weather api
    Former-commit-id: a7b0f1260576765cd9028c649f88b27fb6177c6f

--- backend/information/urls.py
+++ backend/information/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import weatherSunsetAPIView
 
 urlpatterns=[
-    path('weatherSunset/',weatherSunsetAPIView.as_view()),
+    path('weatherSunset/<float:lat>/<float:lon>',weatherSunsetAPIView.as_view()),
 ]
