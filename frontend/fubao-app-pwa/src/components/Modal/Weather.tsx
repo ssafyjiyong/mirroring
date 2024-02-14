4893ccaaebbdf970d15794aab26afce4bc7a0751
김민영 <jnu195260@gmail.com>
2024-02-14 10:44:56 +0900

    Temp: update weather modal
    Former-commit-id: c56de88b7cfbc44f6cfea39986e47dfe329d6997

--- frontend/fubao-app-pwa/src/components/Modal/Weather.tsx
+++ frontend/fubao-app-pwa/src/components/Modal/Weather.tsx
@@ -27,50 +27,49 @@ import ModalClose from "@mui/joy/ModalClose";
 import Typography from "@mui/joy/Typography";
 import Sheet from "@mui/joy/Sheet";
 import { useQuery } from "@tanstack/react-query";
+import Table from '@mui/joy/Table';
 
-interface Weather{
+interface Weather {
   //온도
-  TMP:{
-    fcstTime:string;
-    fsctValue:String;
+  TMP: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 풍향
-  VEC:{
-    fcstTime:string;
-    fsctValue:String;
+  VEC: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 풍속
-  WSD:{
-    fcstTime:string;
-    fsctValue:String;
+  WSD: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 하늘상태
-  SKY:{
-    fcstTime:string;
-    fsctValue:String;
+  SKY: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 강수확률
-  POP:{
-    fcstTime:string;
-    fsctValue:String;
+  POP: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 파고
-  WAV:{
-    fcstTime:string;
-    fsctValue:String;
+  WAV: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 1시간 강수량
-  PCP:{
-    fcstTime:string;
-    fsctValue:String;
+  PCP: {
+    fcstTime: string;
+    fsctValue: String;
   };
   // 일출
   sunrise: string;
   // 일몰
   sunset: string;
 }
-
-
 
 const Title = styled.p`
   margin: 0.5rem 0.5rem;
@@ -79,60 +78,37 @@ const Title = styled.p`
   color: #000000;
 `;
 
-
-
-function Weather() {
+const Weather = () => {
   const [open, setOpenWeather] = useState<boolean>(false);
   const [weather, setWeather] = useState<Weather | null>(null); // method 상태
-  const lat=34.5436111;
-  const lon=127.4536111;
+  const lat = 34.5436111;
+  const lon = 127.4536111;
 
-  // useEffect(() => {
-    const WeatherPage = async () => {
+  useEffect(() => {
+    const fetchWeather = async () => {
+      try {
+        console.log(lat, lon);
+        const response = await weatherGetApi({ lat, lon });
+        console.log(response["weather"]);
+        setWeather(response);
+      } catch (error) {
+        console.error("API 호출 중 에러 발생:", error);
+      }
+    };
 
-      const { data: weather, isError, isLoading } = useQuery<Weather>({
-        
-        queryKey: ["weatherData"], // Pass query key as part of the options object
-        queryFn: async () => {
-          // const token = localStorage.getItem("token");
-          console.log(lat,lon);
-          const response = await weatherGetApi({lat,lon});
-          console.log(response);
-          return response;
-    
-        },
-      });
-    
-      if (isLoading) {
-        return <p>Loading...</p>;
-      };
-    
-      if (isError || !weather) {
-        return <p>데이터를 가져오지 못해습니다.</p>;
-      };
-
-    //   try {
-    //     const response = await weatherGetApi({lat,lon});
-    //     console.log(response['weather'])
-
-    //   } catch (error) {
-    //     console.error("API 호출 중 에러 발생:", error);
-    //   }
-    // };
-
-    WeatherPage();
-  };
+    fetchWeather();
+  });
 
   return (
     <>
-    <button onClick={() => setOpenWeather(true)}> open modal </button>
-    <Modal
-      open={open}
-      onClose={() => setOpenWeather(false)}
-      sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-    >
-      <ModalDialog>
-      <ModalClose variant="plain" sx={{ m: 1 }} />
+      <button onClick={() => setOpenWeather(true)}> open modal </button>
+      <Modal
+        open={open}
+        onClose={() => setOpenWeather(false)}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <ModalDialog>
+          <ModalClose variant="plain" sx={{ m: 1 }} />
           <Typography
             component="h2"
             id="modal-title"
@@ -145,7 +121,7 @@ function Weather() {
             현재 날씨
           </Typography>
           <Typography
-            component="h6"
+            component="h2"
             id="modal-title"
             level="h4"
             textColor="inherit"
@@ -153,13 +129,37 @@ function Weather() {
             mb={1}
             textAlign={"center"}
           >
-          {weather && (<div>{weather.sunrise}</div>)}
+            {weather && (
+              <div>
+                <div>일출시간 : {weather.sunrise}</div>
+                <div>일몰시간 : {weather.sunset}</div>
+                <table aria-label="basic table">
+                <thead>
+                  <tr>
+                  <th style={{ width: '40%' }}></th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                    <th scope="row">시간</th>
+                    <th scope="row">날씨</th>
+                    <th scope="row">기온</th>
+                    <th scope="row">강수량</th>
+                    <th scope="row">강수확률</th>
+                    <th scope="row">풍속</th>
+                    <th scope="row">파고</th>
+                    <th scope="row">풍향</th>
+                    </tr>
+                  </tbody>
+                  
+                </table>
+              </div>
+            )}
           </Typography>
-
-      </ModalDialog>
-    </Modal>
+        </ModalDialog>
+      </Modal>
     </>
   );
-}
+};
 
 export default Weather;
--- frontend/fubao-app-pwa/src/store/api.js
+++ frontend/fubao-app-pwa/src/store/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000";
-// const API_URL = "https://i10c104.p.ssafy.io/api";
+// const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://i10c104.p.ssafy.io/api";
 const API_URL_FLASK = "https://i10c104.p.ssafy.io/ai";
 
 // GET 요청 API
