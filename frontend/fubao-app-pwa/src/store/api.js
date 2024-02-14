e9e51cad47912dc839e888c6a8592bc280e0e221
오유진 <yjin0414@naver.com>
2024-02-14 09:27:43 +0900

    Add: Add GET Fishlist
    Former-commit-id: 40f2ea25ddd04e8d8841e3687047f4d8db225686

--- frontend/fubao-app-pwa/src/store/api.js
+++ frontend/fubao-app-pwa/src/store/api.js
@@ -155,6 +155,25 @@ export const classifyApiNone = async (file) => {
   }
 };
 
+// Fish GET
+export const fishGetApi = async (token) => {
+  try {
+    const response = await axios.get(`${API_URL}/fish/`, {
+      headers: {
+        Authorization: `Token ${token}`,
+        "Content-Type": "application/json",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(
+      "An error occurred during the API call:",
+      error.response ? error.response.data : error.message
+    );
+    throw new Error("Failed to fetch fish list");
+  }
+};
+
 // PUT 요청 API
 
 // PATCH 요청 API
