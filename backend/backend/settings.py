755097aa63a21b7420ea1ae43dc9d32538b7d3f2
김민영 <jnu195260@gmail.com>
2024-02-14 15:35:01 +0900

    Temp: update dockerfile
    Former-commit-id: 62426d25634824a372d340a76b1bc07e794a57ba

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -173,7 +173,7 @@ STATIC_ROOT = ''
 STATICFILES_DIRS = [
     BASE_DIR / 'profile_images',
 ]
-STATIC_ROOT=os.path.join(BASE_DIR, 'profile_images/')
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
